bccfa03995e01865a0574320d390572d19e67aee
adityaj99 <jadhavaditya102@gmail.com>
2025-09-23 23:37:42 +0530

    Fix CORS for Vercel frontend

--- Server/app.js
+++ Server/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://recipe-project-black.vercel.app",
     credentials: true,
   })
 );
